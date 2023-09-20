5cf44fc5eba9415894b1f8cad3d59f26a1c6d292
Scripsi <scripsi@scripsi.uk>
2023-09-20 17:52:48 +0100

    Update startup script with network check
    Update startup check with network check

--- iamaduck.sh
+++ iamaduck.sh
@@ -1,1 +1,10 @@
 echo "$0 started!" > iamaduck.log
+
+# Test network
+wget -q --spider http://google.com
+
+if [ $? -eq 0 ]; then
+    echo "Internet online" >> iamaduck.log
+else
+    echo "Internet offline" >> iamaduck.log
+fi
