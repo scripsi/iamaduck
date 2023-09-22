e3e52274085c78ad57c4d82a35ff350582e4f92c
Scripsi <duckling@scripsi.uk>
2023-09-22 17:07:43 +0100

    Enable indiCat and shutdown

--- src/iamaduck.py
+++ src/iamaduck.py
@@ -6,7 +6,7 @@ import config
 import schedule
 import time
 from inky.inky_uc8159 import Inky
-from gpiozero import Button
+from gpiozero import Button, LED
 from signal import pause
 import screen_quacks
 import os
@@ -19,11 +19,11 @@ Quack quack!
 """)
 
 inky = Inky()
-button_a = Button(5)
+button_a = Button(5,hold_time=2)
 button_b = Button(6)
 button_c = Button(16)
 button_d = Button(24)
-
+indiCat = LED(23)
 
 
 def show_image(image_to_show):
@@ -36,16 +36,29 @@ def show_image(image_to_show):
 def show_quacks():
     show_image("quacks")
 
+def toggle_indiCat():
+    config.dbg("indiCat toggled!")
+    indiCat.toggle()
 
-button_a.when_released = show_quacks
+def turn_off():
+    config.dbg("Button A held. Shutting down.")
+    indiCat.blink(on_time=0.2,off_time=0.2)
+    time.sleep(2)
+    indiCat.on()
+    os.system("sudo shutdown now")
+
+button_a.when_pressed = toggle_indiCat
+button_a.when_held = turn_off
 button_b.when_released = show_quacks
 button_c.when_released = show_quacks
 button_d.when_released = show_quacks
 
+indiCat.blink()
 config.setup()
 config.dbg("Debugging enabled!")
 screen_quacks.setup()
 show_quacks()
+indiCat.off()
 schedule.every(int(config.ini['screen_quacks']['refresh_interval'])).minutes.do(show_quacks)
 
 while True:
