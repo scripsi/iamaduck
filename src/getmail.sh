3029e4e6666018b3f9fd1f6b7a780e4dcf3f3d9e
Scripsi <scripsi@scripsi.uk>
2024-08-02 17:54:11 +0100

    Switch mailscripts to use mythic-beats server

--- src/getmail.sh
+++ src/getmail.sh
@@ -1,2 +1,3 @@
 # This runs offlineimap to download new mail
-offlineimap -c /mnt/iamaduck/mail.ini
+
+offlineimap -c /mnt/iamaduck/mail.ini -k metadata=~/.offlineimap-pallas -k Repository_Remote:remotehost=pallas.mythic-beasts.com
--- src/randomqoth.sh
+++ src/randomqoth.sh
@@ -2,4 +2,8 @@
 # and then runs offlineimap on motherduck only to upload it
 
 ~/iamaduck/.venv/bin/python ~/iamaduck/src/randomqoth.py
-offlineimap -c /mnt/iamaduck/mail.ini -k Repository_Remote:readonly=False -f qoth
+offlineimap -c /mnt/iamaduck/mail.ini \
+            -k metadata=~/.offlineimap-pallas \
+            -k Repository_Remote:remotehost=pallas.mythic-beasts.com \
+            -k Repository_Remote:readonly=False \
+            -f qoth
